7e0522ff03398980e886b13abe41cf0235005067
Zachary Lincoln <zlincoln365@gmail.com>
2022-06-20 23:18:39 -0400

    Fixed Scripts

--- Node/setup.sh
+++ Node/setup.sh
@@ -1,6 +1,6 @@
-apt install python3-flask -y
-rm -rf /serv/Cluster
-git clone https://github.com/ZacharyLincoln/TempCluster /serv/Cluster
+sudo apt install python3-flask -y
+sudo rm -rf /serv/Cluster
+sudo git clone https://github.com/ZacharyLincoln/TempCluster /serv/Cluster/
 
 crontab -l > tempcron
 echo "@reboot bash /serv/Cluster/Node/start.sh" > tempcron
--- Node/start.sh
+++ Node/start.sh
@@ -1,2 +1,2 @@
 export FLASK_APP=/serv/Cluster/Node/app.py
-flask --host=0.0.0.0 --port=8080
+flask run --host=0.0.0.0 --port=8080
